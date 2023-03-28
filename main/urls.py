0e46388288497f649a28ecdc4e7a58c3b991abe1
Eunduck <silver@Eunduckui-MacBookPro.local>
2023-03-28 23:29:17 +0900

    게시글 작성, 삭제

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	path('', views.index, name='index'),
     path('blog', views.blog),
     path('blog/<int:pk>/', views.detail),
+    path('blog/new', views.new),
+    path('blog/delete/<int:pk>/', views.delete),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Post
 
@@ -13,3 +13,17 @@ def blog(request):
 def detail(request, pk):
 	post = Post.objects.get(pk=pk)
 	return render(request, 'main/detail.html', {'post':post})
+
+def new(request):
+	if request.method == 'POST':
+		new_blog=Post.objects.create(
+			postname = request.POST['postname'],
+			contents = request.POST['contents'],
+		)
+		return blog(request)
+	return render(request, 'main/new.html')
+
+def delete(request, pk):
+	post = Post.objects.get(pk=pk)
+	post.delete()
+	return redirect('/blog')
