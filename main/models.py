acbcf935913120f19f3a44c7cd5fb0a11fe3f0c0
Eunduck <silver@Eunduckui-MacBookPro.local>
2023-03-28 21:58:17 +0900

    게시판 페이지

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+# 장고에서 model을 지원해준다.
 # Create your models here.
+
+class Post(models.Model):
+    postname = models.CharField(max_length=50)
+    contents = models.TextField()
+
+    # 객체를 표현 Post Object(1) 대신 
+    # 다른 형식으로 표현하기 위한 함수
+    def __str__(self):
+        return self.postname
--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('test', views.index, name='index'),
+	path('', views.index, name='index'),
+    path('blog', views.blog),
 ]
